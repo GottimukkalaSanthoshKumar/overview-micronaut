2d0f2a68c55dcf790ec3a6a44a057944525aacb6
santhoshkumar.gottimukkala <santhoshkumar.gottimukkala@indium.tech>
2025-04-07 18:35:09 +0530

    code added

---
+++ .idea/Micronaut/sample-micronaut/src/test/java/com/indium/SampleMicronautTest.java
@@ -0,0 +1,21 @@
+package com.indium;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class SampleMicronautTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
